0cdfca5e48b683c803f32dd94d87ba1b2688bf47
ScottC <devnull@localhost>
2010-04-27 19:43:47 -0500

    added ignore favico.ico

--- MultithreadedCommand.Web/Global.asax.cs
+++ MultithreadedCommand.Web/Global.asax.cs
@@ -21,6 +21,7 @@ namespace MultithreadedCommand.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
 
             routes.MapRoute(
                 "Default",                                              // Route name
